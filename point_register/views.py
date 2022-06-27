083837a6c828bd183408b4cb680b30d0ea7d7cbb
unknown <lakagul.peem@gmail.com>
2022-06-27 23:51:35 +0700

    Complete render list of bakery on html

--- point_register/views.py
+++ point_register/views.py
@@ -1,6 +1,12 @@
+from multiprocessing import context
+from django.template import loader
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Bakery
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    bakery_list = Bakery.objects.all()
+    context = {'bakery_list': bakery_list}
+    return render(request, 'point_register/index.html', context)
