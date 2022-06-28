4dc7368fb0d211b7ebb6f8496c958d0b9db108e9
unknown <lakagul.peem@gmail.com>
2022-06-28 16:50:44 +0700

    Fix button that cannot be click

--- point_register/urls.py
+++ point_register/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'point_register'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('bakery/', views.bakery_detail, name='bakery'),
+    path('setbox/', views.setbox_deatail, name='setbox')
 ]
--- point_register/views.py
+++ point_register/views.py
@@ -8,3 +8,11 @@ from .models import Bakery
 
 def index(request):
     return render(request, 'point_register/index.html')
+
+def bakery_detail(request):
+    bakery_list = Bakery.objects.all()
+    context = {'bakery_list': bakery_list}
+    return render(request, 'point_register/bakery_detail.html', context)
+
+def setbox_deatail(request):
+    pass
