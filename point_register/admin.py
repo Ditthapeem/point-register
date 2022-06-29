9e6a08a7a00278d08f525bfbe87778816d3f7bfc
unknown <lakagul.peem@gmail.com>
2022-06-29 14:22:00 +0700

    Adding type model

--- point_register/admin.py
+++ point_register/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Bakery, Point
+from .models import Bakery, Point, Type
 
 admin.site.register(Bakery)
 admin.site.register(Point)
+admin.site.register(Type)
--- point_register/models.py
+++ point_register/models.py
@@ -8,8 +8,15 @@ class Bakery(models.Model):
         return self.name_text
 
 class Point(models.Model):
-    name = models.ForeignKey(Bakery, on_delete=models.CASCADE, unique=True)
+    name = models.OneToOneField(Bakery, on_delete=models.CASCADE)
     point = models.IntegerField(default=100)
 
     def __str__(self) -> str:
         return str(f'{self.name}: {self.point} points')
+
+class Type(models.Model):
+    name = models.OneToOneField(Bakery, on_delete=models.CASCADE)
+    type_text = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return str(f'{self.name}: Bakery {self.type_text}')
