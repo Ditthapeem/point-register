43bc63039590a28f8a02d02178cf132f264d2849
unknown <lakagul.peem@gmail.com>
2022-06-29 14:54:14 +0700

    Complete render bakery by their type

--- point_register/views.py
+++ point_register/views.py
@@ -3,15 +3,16 @@ from django.template import loader
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Bakery
+from .models import Bakery, Type
 
 
 def index(request):
     return render(request, 'point_register/index.html')
 
 def bakery_detail(request):
-    bakery_list = Bakery.objects.all()
-    context = {'bakery_list': bakery_list}
+    type_list = Type.objects.all()
+    context = { 'list': type_list}
+    print(context)
     return render(request, 'point_register/bakery_detail.html', context)
 
 def setbox_deatail(request):
