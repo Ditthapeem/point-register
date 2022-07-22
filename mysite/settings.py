220c4da824ecd1cd1ed59685a5ebdf4a71ede15c
unknown <lakagul.peem@gmail.com>
2022-07-22 15:31:39 +0700

    Deploy 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,8 +82,8 @@ db_from_env = dj_database_url.config(conn_max_age=600)
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
 
